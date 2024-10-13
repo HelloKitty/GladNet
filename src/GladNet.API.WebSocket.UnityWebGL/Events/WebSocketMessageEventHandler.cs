6c5a0c2bfd0e2bb7aeead02750a3c49288111d2c
HelloKitty <andrew.blakely@ymail.com>
2024-10-12 20:11:18 -0500

    Create WebSocketMessageEventHandler.cs

---
+++ src/GladNet.API.WebSocket.UnityWebGL/Events/WebSocketMessageEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace GladNet
+{
+	/// <summary>
+	/// Delegate to handle WebSocket message events.
+	/// This delegate will be triggered when a message is received via the WebSocket connection.
+	/// </summary>
+	/// <param name="data">The message data received, represented as a byte array.</param>
+	public delegate void WebSocketMessageEventHandler(byte[] data);
+}
