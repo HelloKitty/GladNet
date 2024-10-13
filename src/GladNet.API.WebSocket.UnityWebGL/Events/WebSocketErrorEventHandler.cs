589052745f0219679df06ce7daad5120986cc1d3
HelloKitty <andrew.blakely@ymail.com>
2024-10-12 20:11:20 -0500

    Create WebSocketErrorEventHandler.cs

---
+++ src/GladNet.API.WebSocket.UnityWebGL/Events/WebSocketErrorEventHandler.cs
@@ -0,0 +1,9 @@
+﻿namespace GladNet
+{
+	/// <summary>
+	/// Delegate to handle WebSocket error events.
+	/// This delegate will be triggered when an error occurs on the WebSocket connection.
+	/// </summary>
+	/// <param name="errorMsg">The error message describing the issue encountered.</param>
+	public delegate void WebSocketErrorEventHandler(string errorMsg);
+}
