8487a188a46ce71b8fa9c655f1a9da50c862950f
HelloKitty <andrew.blakely@ymail.com>
2024-10-12 20:11:16 -0500

    Create WebSocketOpenEventHandler.cs

---
+++ src/GladNet.API.WebSocket.UnityWebGL/Events/WebSocketOpenEventHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace GladNet
+{
+	/// <summary>
+	/// Delegate to handle WebSocket open events.
+	/// This delegate will be triggered when the WebSocket connection is successfully opened.
+	/// </summary>
+	public delegate void WebSocketOpenEventHandler();
+}
